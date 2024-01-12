8510e326930704e615d97e9453d8c15b75e61f68
DUVANPS <jaiderpedroza20@gmail.com>
2024-01-12 17:35:19 -0500

    mi-tarea

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        
+        <a href="https://react.dev" target="_blank">
+          <img src="/src/img/anonymous-organizacion.jpg" className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Hola Supuesta Leon Elizabeth </h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          DISPOSITIVO HACKEADO CORRECTAMENTE
+        </button>
+        <p>
+          Un gusto soy ander condinador de asuntos de civerseguridad de Facebook, tu dispositivo a sido hackeado por flagio en pocos mitutos saldra a la luz toda tu info personal creo que mereses una lección por lo mal@ que eres por favor disculpa pero es mi trabajo.
+        </p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
